b14f774e7893b0cee6d4db30234f1992fb7d2425
» Kezuk <54121447+kezukdev@users.noreply.github.com>
2023-01-07 00:36:29 +0100

    Full ladders command finished

--- src/main/java/saturne/practice/handler/command/LadderCommand.java
+++ src/main/java/saturne/practice/handler/command/LadderCommand.java
@@ -1,7 +1,5 @@
 package saturne.practice.handler.command;
 
-import java.io.IOException;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,6 +9,7 @@ import org.bukkit.entity.Player;
 import net.md_5.bungee.api.ChatColor;
 import saturne.practice.Main;
 import saturne.practice.ladder.LadderType;
+import saturne.practice.utils.BukkitSerialization;
 
 public class LadderCommand implements CommandExecutor {
 	
@@ -49,9 +48,50 @@ public class LadderCommand implements CommandExecutor {
 				return false;
 			}
 			fileConfig.set("ladders", args[1]);
-			fileConfig.set("ladders." + args[1], args[2]);
+			fileConfig.createSection("ladders." + args[1] + ".type");
+			fileConfig.set("ladders." + args[1] + ".type", args[2]);
+			fileConfig.createSection("ladders." + args[1] + ".inventory");
+			fileConfig.createSection("ladders." + args[1] + ".icon");
+			fileConfig.createSection("ladders." + args[1] + ".displayname");
 			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.main.getConfig().getString("messages.created-ladder").replace("%ladderName%", args[1]).replace("%ladderType%", args[2])));
 			return false;
+		}
+		final Player player = (Player) sender;
+		if (args[0].equalsIgnoreCase("setinv")) {
+			if (args.length < 2 || args.length > 2) {
+				sender.sendMessage(ChatColor.LIGHT_PURPLE + "/ladder setinv <name>");
+				return false;
+			}
+			if (!fileConfig.contains("ladders." + args[1])) {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.main.getConfig().getString("messages.ladder-doesnt-exist").replace("%ladderName%", args[1])));
+				return false;
+			}
+			fileConfig.set("ladders." + args[1] + ".inventory", BukkitSerialization.itemStackArrayToBase64(player.getInventory().getContents()));
+			sender.sendMessage(ChatColor.GREEN + "The inventory was correctly defined!");
+		}
+		if (args[0].equalsIgnoreCase("seticon")) {
+			if (args.length < 2 || args.length > 2) {
+				sender.sendMessage(ChatColor.LIGHT_PURPLE + "/ladder seticon <name>");
+				return false;
+			}
+			if (!fileConfig.contains("ladders." + args[1])) {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.main.getConfig().getString("messages.ladder-doesnt-exist").replace("%ladderName%", args[1])));
+				return false;
+			}
+			fileConfig.set("ladders." + args[1] + ".icon", player.getItemInHand().getType().toString());
+			sender.sendMessage(ChatColor.GREEN + "The icon has been defined to " + player.getItemInHand().getType().toString());
+		}
+		if (args[0].equalsIgnoreCase("setdisplay")) {
+			if (args.length < 3 || args.length > 3) {
+				sender.sendMessage(ChatColor.LIGHT_PURPLE + "/ladder setdisplay <name> <display>");
+				return false;
+			}
+			if (!fileConfig.contains("ladders." + args[1])) {
+				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.main.getConfig().getString("messages.ladder-doesnt-exist").replace("%ladderName%", args[1])));
+				return false;
+			}
+			fileConfig.set("ladders." + args[1] + ".displayName", args[2]);
+			sender.sendMessage(ChatColor.GREEN + "The displayname has been edited to " + args[2] + "!");
 		}
 		return false;
 	}
