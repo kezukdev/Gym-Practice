c70ef3ffb5d58a29de90ab36f462b0bdfc12c0c8
» Kezuk <54121447+kezukdev@users.noreply.github.com>
2023-02-04 14:16:38 +0100

    Aether added

---
+++ src/main/java/com/bizarrealex/aether/scoreboard/cooldown/BoardFormat.java
@@ -0,0 +1,11 @@
+package com.bizarrealex.aether.scoreboard.cooldown;
+
+public enum BoardFormat
+{
+    SECONDS("SECONDS", 0), 
+    MINUTES("MINUTES", 1), 
+    HOURS("HOURS", 2);
+    
+    private BoardFormat(final String s, final int n) {
+    }
+}
